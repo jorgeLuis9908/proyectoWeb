ea07ad4c6661cb1524fb67a8e341d25dc1913191
jorgeLuis9908 <84887960+jorgeLuis9908@users.noreply.github.com>
2021-08-09 14:42:01 -0500

    Crear archivo Ventas_GitHub.cs

---
+++ IdentityManager/Ventas_GitHub.cs
@@ -0,0 +1,25 @@
+int filasel= tabla2.getSelectedRow();
+        int confirmado = JOptionPane.showConfirmDialog(this,"Estaras seguro");
+        if (JOptionPane.OK_OPTION == confirmado){
+           String  cod=(String)tabla2.getValueAt(filasel, 0);
+             String eliminarSQL="DELETE FROM primeroA WHERE codigo_inscripcion= '"+cod+"'";
+               String eliminarSQL1="DELETE FROM segundoB WHERE codigo_inscripcion= '"+cod+"'";
+            String eliminarSQL2="DELETE FROM datosAlumnos WHERE codigo_inscripcion= '"+cod+"'";
+                   try {
+             PreparedStatement pst  = cn.prepareStatement(eliminarSQL);
+                 PreparedStatement ps = cn.prepareStatement(eliminarSQL2);
+                   PreparedStatement psM = cn.prepareStatement(eliminarSQL1);
+                          
+          pst.executeUpdate();
+           ps.executeUpdate();
+           psM.executeUpdate();
+     
+            JOptionPane.showMessageDialog(null, "Borrado");
+         
+            } 
+            catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            }
+        }else{
+            System.out.println("Eliminación cancelada.");
+        }
